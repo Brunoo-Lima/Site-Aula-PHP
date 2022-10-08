665be59fd74b0d39bcfcae768dd961a2bcc5eb78
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2022-10-08 18:16:35 -0300

    updates

---
+++ modulos/cliente_model.php
@@ -0,0 +1,39 @@
+class Cliente{
+    private $cli_id;
+    private $cli_nome;
+    private $cli_email;
+    private $cli_sexo;
+
+    function getCli_id() {
+        return $this->cli_id;
+    }
+
+    function getCli_nome() {
+        return $this->cli_id;
+    }
+    
+    function getCli_email() {
+        return $this->cli_id;
+    }
+
+    function getCli_sexo() {
+        return $this->cli_id;
+    }
+
+    function setCli_id($cli_id) {
+        $this->cli_id = $cli_id;
+    }
+
+    function setCli_nome($cli_nome) {
+        $this->cli_nome = $cli_nome;
+    }
+
+    function setCli_email($cli_email) {
+        $this->cli_email = $cli_email;
+    }
+
+    function setCli_sexo($cli_sexo) {
+        $this->cli_sexo = $cli_sexo;
+    }
+
+}
